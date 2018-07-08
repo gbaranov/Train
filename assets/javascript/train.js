3ead8efe72c90f3ecd313806649e5eba62dfd740
Georgy Baranov <floridavbv@gmail.com>
2018-07-07 18:38:36 -0600

    m

---
+++ assets/javascript/train.js
@@ -0,0 +1,17 @@
+var config = {
+    apiKey: "AIzaSyCMUbb3HBLImQmJOsCf-56ztTMusW6dTtE",
+    authDomain: "project-c000e.firebaseapp.com",
+    databaseURL: "https://project-c000e.firebaseio.com",
+    projectId: "project-c000e",
+    storageBucket: "project-c000e.appspot.com",
+    messagingSenderId: "98833963738"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var trains = {
+    
+}
+function setTrain() {
+
+}
